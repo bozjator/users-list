bd675fc4e7908229f25158cbe893892470600c81
Blaz Bozja <bozjator@gmail.com>
2024-12-21 14:20:40 +0100

    feat: init ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { appEffects, appReducers } from './shared/store/app-store';
+
+const storeConfig = [
+  provideStore(appReducers),
+  provideEffects(appEffects),
+  provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    ...storeConfig,
   ],
 };
